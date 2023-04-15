e56b33ef80f174e93e6e0b872fc566a785d81b4d
Aisha <128395400+Aishaa15@users.noreply.github.com>
2023-04-15 15:32:21 +0000

    inialize object in the main class

--- main.java
+++ main.java
@@ -2,7 +2,8 @@ public class main
 {
     public static void main(String[] args)
     {
-        
+        // create new instance of IntegerVerifier class
+        verifyInteger verifier = new verifyInteger();
         
     }
     
